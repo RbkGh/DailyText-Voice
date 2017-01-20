666219444ef92f16ba6679990f762d27e1ab3d5a
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-20 23:32:44 +0000

    Added Notification Activity Handler

---
+++ app/src/main/java/com/swiftpot/dailytextvoice/NotificationActivityHandler.java
@@ -0,0 +1,13 @@
+package com.swiftpot.dailytextvoice;/**
+ * Created by Ace Programmer Rbk<rodney@swiftpot.com> on 20-Jan-17
+ * 11:30 PM
+ */
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ace Programmer Rbk<rodney@swiftpot.com> on 20-Jan-17
+ * 11:30 PM
+ */
+public class NotificationActivityHandler extends AppCompatActivity {
+}

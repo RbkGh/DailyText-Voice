3d62fc1fd0d96ef08e7dd78e1099239528b4ea8b
Ace P Rbk <rbk.unlimited@gmail.com>
2017-01-20 23:34:13 +0000

    Added oncreate with dummy data

--- app/src/main/java/com/swiftpot/dailytextvoice/NotificationActivityHandler.java
+++ app/src/main/java/com/swiftpot/dailytextvoice/NotificationActivityHandler.java
@@ -3,11 +3,27 @@ package com.swiftpot.dailytextvoice;/**
  * 11:30 PM
  */
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * Created by Ace Programmer Rbk<rodney@swiftpot.com> on 20-Jan-17
  * 11:30 PM
  */
 public class NotificationActivityHandler extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String action= (String)getIntent().getExtras().get("DO");
+        Log.i("LOG", "lauching action: " + action);
+        if(action.equals("1")){
+        } else if(action.equals("2")){
+        } else if(action.equals("config")){
+
+        }
+    }
 }
